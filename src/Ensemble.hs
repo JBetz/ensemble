94b697970003f25afb93e13fda3b9f44606447eb
Joe Betz <joebetz91@gmail.com>
2023-01-27 18:39:45 +0100

    remove fluidsynth until cross-compilation issues are resolved

--- src/Ensemble.hs
+++ src/Ensemble.hs
@@ -1,15 +1,11 @@
-{-# LANGUAGE BangPatterns #-}
-
 module Ensemble 
   ( module Ensemble.API
   , module Ensemble.Engine
   , module Ensemble.Handler 
   , module Ensemble.Sequencer
-  , module Ensemble.Soundfont
   ) where
 
 import Ensemble.API
 import Ensemble.Engine
 import Ensemble.Handler
 import Ensemble.Sequencer
-import Ensemble.Soundfont hiding (process)

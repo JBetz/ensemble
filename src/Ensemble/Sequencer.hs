ca4bb7acdc2305fea96f2f6155409ec6c0381cc0
Joe Betz <joebetz91@gmail.com>
2023-02-03 04:59:20 +0100

    add prefix to Tick newtype to avoid namespace conflict

--- src/Ensemble/Sequencer.hs
+++ src/Ensemble/Sequencer.hs
@@ -18,7 +18,7 @@ data Sequencer = Sequencer
     , sequencer_clients :: IORef (Map String EventCallback)
     }
 
-newtype Tick = Tick { value :: Int }
+newtype Tick = Tick { tick_value :: Int }
     deriving (Eq, Ord, Show, Enum)
 
 createSequencer :: IO Sequencer

84f8c9bdc1a2e6091937c64bd9f02e4ab43d4800
Joe Betz <joebetz91@gmail.com>
2023-02-09 17:11:55 +0100

    soundfont: add bank number and program number to instrument state

--- src/Ensemble/Instrument.hs
+++ src/Ensemble/Instrument.hs
@@ -30,6 +30,8 @@ data SoundfontInstrument = SoundfontInstrument
     { soundfontInstrument_soundfont :: Soundfont
     , soundfontInstrument_settings :: FluidSettings
     , soundfontInstrument_synth :: FluidSynth
+    , soundfontInstrument_bankNumber :: Int
+    , soundfontInstrument_programNumber :: Int
     } deriving (Show)
 
 data ClapInstrument = ClapInstrument

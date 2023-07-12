4ae341e9fa4d95f487a025dfed1e18b032758409
Joe Betz <joebetz91@gmail.com>
2023-07-13 00:01:32 +0200

    add POSIX stubs

--- src/Ensemble/Window/POSIX.hs
+++ src/Ensemble/Window/POSIX.hs
@@ -1,1 +1,12 @@
 module Ensemble.Window.POSIX where
+
+import Foreign.Ptr
+
+showWindow :: Ptr () -> IO ()
+showWindow = undefined
+
+createParentWindow :: Maybe (Ptr ()) -> String -> Int -> Int -> IO (Ptr ())
+createParentWindow _maybeParent _name _width _height = undefined
+
+messagePump :: IO ()
+messagePump = undefined
